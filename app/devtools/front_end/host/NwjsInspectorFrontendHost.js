3a658b5df349df715f48a317c260c5c114059ec7
Max Brosnahan <maximilianbrosnahan@gmail.com>
2015-02-17 18:53:23 +1100

    Add injection of overrides

---
+++ app/devtools/front_end/host/NwjsInspectorFrontendHost.js
@@ -0,0 +1,1 @@
+../../../devtools_overrides/NwjsInspectorFrontendHost.js
--- scripts/build_generated_scripts.js
+++ scripts/build_generated_scripts.js
@@ -1,6 +1,8 @@
 /* jshint evil:true */
 /* global InspectorBackendClass */
 var fs = require('fs'),
+    execSync = require('child_process').execSync,
+    path = require('path'),
     protocol = require(__dirname + '/../app/devtools/protocol.json');
 
 // Mock objects
@@ -24,4 +26,25 @@ fs.writeFileSync(__dirname + '/../app/devtools/front_end/InspectorBackendCommand
 var supportedCSSPropertiesContent = 'WebInspector.CSSMetadata.initializeWithSupportedProperties({});'
 fs.writeFileSync(__dirname + '/../app/devtools/front_end/SupportedCSSProperties.js', supportedCSSPropertiesContent);
 
+// Devtools Overrides
+var appBasePath = path.resolve(__dirname, '..', 'app')
+var customFrontEndHostFileName = "NwjsInspectorFrontendHost.js"
+var frontEndModuleConfigFilePath = appBasePath + '/devtools/front_end/host/module.json'
+var frontEndModuleConfig = require(frontEndModuleConfigFilePath)
 
+if(frontEndModuleConfig.scripts.indexOf(customFrontEndHostFileName) === -1){
+  frontEndModuleConfig.scripts.unshift(customFrontEndHostFileName)
+}
+
+fs.writeFileSync(
+  frontEndModuleConfigFilePath,
+  JSON.stringify(frontEndModuleConfig, null, 4) + "\n"
+)
+
+var overrideFilePath = '../../../devtools_overrides/NwjsInspectorFrontendHost.js'
+var linkPath = appBasePath + '/devtools/front_end/host/NwjsInspectorFrontendHost.js'
+var symlinkCommand = "ln -s " +  overrideFilePath + ' ' + linkPath
+
+if(!fs.existsSync(linkPath)){
+  execSync(symlinkCommand)
+}
